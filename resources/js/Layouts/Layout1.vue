99bbd5cf6ac35c7d900e5a7cd908a2547cdeaccb
XcviiNB <nathanielbatausafgo@gmail.com>
2023-09-04 08:48:34 +0800

    initial commit

---
+++ resources/js/Layouts/Layout1.vue
@@ -0,0 +1,40 @@
+<script setup>
+    import MainNav from '@/Components/MainNav.vue'
+    import { ref } from 'vue';
+
+    let width = ref('w-[250px]')
+    let hidden = ref("")
+
+    function toggleWidth() {
+        if(width.value == 'w-[250px]') {
+            width.value = 'w-[50px]'
+            hidden.value = "hidden"
+        }else {
+            width.value = ['w-[250px]']
+            hidden.value = ""
+        }
+    }
+</script>
+
+<template>
+
+<div class="flex min-h-screen">
+    <div id="sidebar" class="bg-blue-900 p-6 duration-500" :class="width" style="position: relative;">
+        <button class="text-xl text-gray-600" @click="toggleWidth" style="position: absolute; right: 10px; top:10px">
+            <i class="fa-solid fa-bars text-blue-500"></i>
+        </button>
+        <div id="branding" :class="hidden">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUMLiKc8WCsotxfJELuuMgOBODCEa9wiGYvw&usqp=CAU" alt="Logo"
+                class="w-[170px] h-[170px] mx-auto rounded-full object-cover">
+            <h1 class="text-3xl text-center text-blue-300 my-6">Amazon Smirks</h1>
+            <MainNav></MainNav>
+        </div>
+        <hr class="border-blue-600">
+
+    </div>
+    <div id="container" class="flex-1">
+        <slot />
+    </div>
+</div>
+
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import Layout1 from '@/Layouts/Layout1.vue'
+
+</script>
+
+<template>
+
+<Layout1>
+    <div class="p-8">
+        <h1 class="text-4xl">Home</h1>
+        <p>
+            Amazon Smirks, a fictional e-commerce giant, has revolutionized the way we shop and interact with technology. Just like its real-world counterpart, Amazon Smirks boasts a vast online marketplace where customers can find virtually anything they desire, from electronics and fashion to groceries and entertainment. With its user-friendly interface and cutting-edge recommendation algorithms, Amazon Smirks tailors the shopping experience to each individual, making it easier than ever to discover new products that align with your interests and preferences.
+        </p>
+        <p>
+            But Amazon Smirks doesn't stop at just online shopping. This innovative company has also ventured into the world of streaming and entertainment, offering a wide range of exclusive content through Smirks Prime Video. Customers can binge-watch their favorite TV series, catch the latest blockbuster movies, and even access original content produced by Amazon Smirks Studios. With its lightning-fast delivery services and commitment to customer satisfaction, Amazon Smirks has become a household name synonymous with convenience, entertainment, and technological innovation.
+        </p>
+    </div>
+</Layout1>
+
+</template>
